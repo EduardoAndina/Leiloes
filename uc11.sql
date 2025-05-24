72b5cee7d414e3eef5f42cb41ba2d43892bb046b
Eduardo Andina <eduardoandinadealmeida.2018@gmail.com>
2025-05-24 04:05:03 -0300

    Script do Banco de Dados atualizado, agora o correto

--- uc11.sql
+++ uc11.sql
@@ -1,38 +1,73 @@
-CREATE DATABASE GameStock;
-Use GameStock;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Abr-2023 às 17:58
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
 
-CREATE TABLE funcionarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    cargo VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE jogo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    plataforma VARCHAR(50) NOT NULL,
-    preco DECIMAL(10,2),
-    quantidade INT NOT NULL
-);
-
-CREATE TABLE vendas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    idJogo INT NOT NULL,
-    categoria VARCHAR(50) NOT NULL,
-    quantidadeVendida INT NOT NULL,
-    valorPago DECIMAL(10,2) NOT NULL,
-    nomeCliente VARCHAR(100) NOT NULL,
-    telefoneCliente VARCHAR(20),
-    dataVenda DATETIME NOT NULL,
-    FOREIGN KEY (idJogo) REFERENCES jogo(id)
-);
-
-SELECT * FROM jogo;
-
-ALTER TABLE funcionarios DROP COLUMN cargo;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-ALTER TABLE vendas
-ADD COLUMN nome_jogo VARCHAR(100) NOT NULL;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `uc11`
+--
+
+CREATE DATABASE uc11;
+USE uc11;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nome` text DEFAULT NULL,
+  `valor` int(11) DEFAULT NULL,
+  `status` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id`, `nome`, `valor`, `status`) VALUES
+(2, 'PS4', 1500, 'Vendido'),
+(3, 'Xbox 360', 800, 'Vendido'),
+(4, 'Iphone 12', 4800, 'Vendido'),
+(5, 'PS2', 400, 'A Venda');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
