b9ecb12cc8a250b04d34e65143a3683501da0762
Eduardo Andina <eduardoandinadealmeida.2018@gmail.com>
2025-05-24 05:07:59 -0300

    Botão de salvar cadastrando produto

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -8,11 +8,12 @@
  * @author Adm
  */
 
-import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 
 
 public class ProdutosDAO {
@@ -23,11 +24,25 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+         String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+        System.out.println("Produto cadastrado com sucesso!");
+
+        prep.close();
+        conn.close();
+    } catch (SQLException erro) {
+        System.err.println("Erro ao cadastrar produto: " + erro.getMessage());
+    }
+              
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
